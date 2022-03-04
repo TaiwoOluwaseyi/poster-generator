c9a479e5a95abfe84cf0895bfadaa09805582971
Mabel Taiwo <seyitaiwo21@gmail.com>
2022-03-04 17:32:19 +0100

    another commit

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
-function generate () {
+function generate() {
   document.getElementById("render").innerHTML = "";
-} 
+  html2canvas(document.querySelector("#capture")).then((canvas) => {
+    document.getElementById("render").appendChild(canvas);
+  });
+}
+
+function download() {
+  const download = document.getElementById("download");
+  let image = document
+    .querySelector("canvas")
+    .toDataURL("image/png")
+    .replace("image/png", "image/octet-stream");
+  download.setAttribute("href", image);
+}
+
+document.querySelector("textarea").addEventListener("keyup", function () {
+  const quoteText = document.getElementById("quote__text");
+
+  if (this.value != "") {
+    quoteText.innerHTML = this.value;
+    renderCanvas();
+  } else {
+    quoteText.innerHTML = "Start typing </br>:)";
+  }
+});
